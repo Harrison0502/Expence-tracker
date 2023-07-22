06229437a79781b7a07d330d78d769d21f336ed0
harrison0502 <harrison0502@gmail.com>
2023-07-23 00:26:55 +0800

    feat:"add mongoose"

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
-// setting template engine
+require('./config/mongoose')
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-// setting routes
 app.get('/', (req, res) => {
   res.render('index')
 })
-// starts the express server and listening for connections.
+
 app.listen(port, () => {
   console.log(`Express app listening on port ${port}.`)
 })
