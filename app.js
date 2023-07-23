ec261ba0b3c186cb0bef67741ae48665ded7b3e6
harrison0502 <harrison0502@gmail.com>
2023-07-23 15:31:17 +0800

    feat:add express routes

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 require('./config/mongoose')
+const routes=require('./routes')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`Express app listening on port ${port}.`)
---
+++ routes/modules/home.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const Record = require('../../models/record')
+const User = require('../../models/user')
+
+
+
+router.get('/',(req,res)=>{
+  Record.find()
+  .lean()
+  .then(records=>res.render('index', {records} ))
+  .catch(error=>console.error(error))
+})
+
+module.exports = router
