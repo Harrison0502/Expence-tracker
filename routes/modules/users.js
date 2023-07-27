090812bf5ea0c19371a18ed722d9ea9480bb506b
harrison0502 <harrison0502@gmail.com>
2023-07-27 10:10:56 +0800

    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -10,18 +10,41 @@ router.use(bodyParser.urlencoded({ extended: true }))
 
 router.get('/login', async (req, res) => {
   try {
-    res.render('login');
+    res.render('login')
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
 })
 
 router.get('/register', async (req, res) => {
   try {
-    res.render('register');
+    res.render('register')
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
 })
 
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email }).then(user => {
+    if (user) {
+      console.log('User already exists.')
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    } else {
+      return User.create({
+        name,
+        email,
+        password
+      })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    }
+  })
+    .catch(err => console.log(err))
+})
 module.exports = router
